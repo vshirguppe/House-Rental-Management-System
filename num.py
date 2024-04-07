f9f048e70e2083b30aab31022885774166dc33c7
Vrushabh Shirguppe <97335988+vshirguppe@users.noreply.github.com>
2024-04-08 00:31:52 +0530

    Add files via upload

---
+++ num.py
@@ -0,0 +1,11 @@
+def second(ar):
+    if (ar[0]<ar[1]):
+        if(ar[1]<ar[2]):
+            print("Second Largest Number Is :",ar[2])
+        else:
+            pass
+    else:
+        print("No Second Lrgest number")
+second(ar=[1,2,3,4])
+
+
